ffb1840abebc4d9428625e8a99c4f9d384be4948
AliBharwani <ali.e.bharwani@gmail.com>
2022-06-23 00:09:47 -0700

    Add nn search to kdtree

--- Assets/Scripts/KDTree.cs
+++ Assets/Scripts/KDTree.cs
@@ -9,8 +9,9 @@ public class KDTree
     {
         public Node left;
         public Node right;
-        public float[] data;
-        public Node(float[] _data)
+        public double[] data;
+        public bool isLeaf;
+        public Node(double[] _data)
         {
             data = _data;
         }
@@ -19,15 +20,17 @@ public class KDTree
     // extraData represents the num of floats at the end that should be ignored
     private int k;
     private int extraData;
-    private List<float[]> values;
+    private List<double[]> values;
     private Node root;
+    private double currentBestDist;
+    private Node closest;
     public KDTree (int _k = 30, int _extraData = 2)
     {
         k = _k;
         extraData = _extraData;
-        values = new List<float[]>();
+        values = new List<double[]>();
     }
-   public void Add(float[] entry)
+   public void Add(double[] entry)
     {
         values.Add(entry);
     }
@@ -35,32 +38,107 @@ public class KDTree
 
     public void Build()
     {
-        root = recurs(0, values);
+        root = recursiveBuild(0, values);
         values = null;
     }
-    private Node recurs(int depth , List<float[]> values)
+    private Node recursiveBuild(int depth , List<double[]> values)
     {
         if (values.Count == 0)
             return null;
         int axis = depth % k;
-        values.Sort(delegate (float[] x, float[] y)
+        values.Sort(delegate (double[] x, double[] y)
         {
             return x[axis].CompareTo(y[axis]);
         });
         int median_idx = values.Count / 2;
         Node newNode = new Node(values[median_idx]);
         int numValuesAfterMedian = values.Count - (median_idx + 1);
-        List<float[]> valuesLeft = values.GetRange(0, median_idx);
-        List<float[]> valuesRight = values.GetRange(median_idx + 1, numValuesAfterMedian);
-        newNode.left = recurs(depth + 1, valuesLeft);
-        newNode.right = recurs(depth + 1, valuesRight);
+        List<double[]> valuesLeft = values.GetRange(0, median_idx);
+        List<double[]> valuesRight = values.GetRange(median_idx + 1, numValuesAfterMedian);
+        newNode.left = recursiveBuild(depth + 1, valuesLeft);
+        newNode.right = recursiveBuild(depth + 1, valuesRight);
+        newNode.isLeaf = newNode.left == null && newNode.right == null;
+        //if (!newNode.isLeaf && (newNode.left == null || newNode.right == null))
+        //{
+        //    Debug.Log("CORNER CASE FOUND! ");
+        //}
         return newNode;
     }
 
-    public float[] nnSearch(float[] searchVector )
+    public double[] nnSearch(double[] searchVector, int depth = 0 )
     {
-        return new float[0];
+        closest = null;
+        recursiveNNSearch(root, searchVector, depth);
+        return closest.data;
     }
 
+    private void recursiveNNSearch(Node node, double[] searchVector, int depth) 
+    {
+        if (node == null)
+            return;
+        int axis = depth % k;
+        // if the bounding box is too far, do nothing 
+        //if (closest != null && Math.Pow(node.data[axis] - searchVector[axis], 2) > currentBestDist)
+        //    return;
+        double dist = distanceBetween(searchVector, node.data);
+        if (closest == null || dist < currentBestDist)
+        {
+            closest = node;
+            currentBestDist = dist;
+        }
+        if (searchVector[axis] < node.data[axis])
+        {
+            // search left first
+            recursiveNNSearch(node.left, searchVector, depth + 1);
+            // do the weird hypersphere thing wikipedia talked about lol
+            // ok but really: 
+            // If the distance between the node's value at the splitting dimension and the search vector's va
+            if (distBetweenAtAxis(node.data, searchVector, axis) < currentBestDist)
+            {
+                recursiveNNSearch(node.right, searchVector, depth + 1);
+            }
+        } else
+        {
+            // search right first
+            recursiveNNSearch(node.right, searchVector, depth + 1);
+            if (distBetweenAtAxis(node.data, searchVector, axis) < currentBestDist)
+            {
+                recursiveNNSearch(node.left, searchVector, depth + 1);
+            }
+        }
+    }
+    private double distBetweenAtAxis(double[] a, double[] b, int axis)
+    {
+        return Math.Pow(a[axis] - b[axis], 2);
+    }
+    private double distanceBetween(double[] a, double[] b)
+    {
+        // use squared euclidan distance to avoid having to calculate square roots
+        double answer = 0;
+        for (int i = 0; i < k; i++)
+        {
+            answer += Math.Pow(a[i] - b[i], 2);
+        }
+        return answer;
+    }
 
+    public double[] bruteForceSearch(double[] searchVector)
+    {
+        closest = null;
+        recursiveBruteForceSearch(root, searchVector);
+        return closest.data;
+    }
+    private void recursiveBruteForceSearch(Node node, double[] searchVector)
+    {
+        if (node == null)
+            return;
+        double dist = distanceBetween(searchVector, node.data);
+        if (closest == null || dist < currentBestDist)
+        {
+            closest = node;
+            currentBestDist = dist;
+        }
+        recursiveBruteForceSearch(node.left , searchVector);
+        recursiveBruteForceSearch(node.right, searchVector);
+    }
 }
--- Assets/Scripts/MotionMatching.cs
+++ Assets/Scripts/MotionMatching.cs
@@ -7,13 +7,45 @@ using System.Text;
 
 public class MotionMatching : MonoBehaviour
 {
-
+    double[] means = new double[] {
+        -0.0458957508171,
+        0.148889939955,
+        -1.98212023408,
+        -0.0303706152439,
+        0.147394518158,
+        -2.00615341094,
+        -0.00105541224217,
+        0.000328890938174,
+        0.0011302420994,
+        -0.00187898262142,
+        3.8678468464e-05,
+        0.00122720085238,
+        -0.00165934029416,
+        -0.000206693790398,
+        0.00128557976763,
+        0.898474555729,
+        16.7989438667,
+        182.212187285,
+        188.200992116,
+        267.64185031,
+        0.896308134437,
+        16.8000879095,
+        182.354338485,
+        188.366045526,
+        267.646277596,
+        0.893661161189,
+        16.8010779027,
+        182.299978261,
+        188.581469405,
+        267.647131713
+    };
+    double[] std_devs = new double[] { 2.75672803451, 0.0851475658313, 5.39294074082, 2.76605976978, 0.0867144467634, 5.404615269, 1.50215804108, 0.671604269949, 1.99188466153, 1.52159098328, 0.687993202854, 2.0142738406, 1.02034196113, 0.29502827464, 1.43598251437, 2.73879165649, 5.38729029753, 175.25471024, 107.414065461, 8.47444261664, 2.74219568686, 5.40109608084, 175.25317976, 107.38595772, 8.47702118087, 2.74476959155, 5.41456517415, 175.261603812, 107.328262296, 8.47962818468 };
 
     private void ingestMotionMatchingDB()
     {
         // @ escapes the backslashes
         string pathToData = @"D:/Unity/Unity 2021 Editor Test/Python/pyoutputs/";
-        // Lol I have to build a KD tree to load the data into
+
         string[] prefixes = {
             "walk1_subject1",
             "walk1_subject2",
@@ -25,6 +57,7 @@ public class MotionMatching : MonoBehaviour
             "run2_subject1",
             "sprint1_subject2",
         };
+        DateTime startTime = DateTime.Now;
         KDTree motionDB = new KDTree();
         int counter = 0;
         foreach (string prefix in prefixes)
@@ -32,13 +65,25 @@ public class MotionMatching : MonoBehaviour
             foreach (string line in File.ReadLines(pathToData + prefix + "_normalized_outputs.txt"))
             {
                 string[] stringValues = line.Split(',').ToArray();
-                motionDB.Add(Array.ConvertAll(stringValues, float.Parse));
+                motionDB.Add(Array.ConvertAll(stringValues, double.Parse));
                 counter++;
             }
         }
+        DateTime ingestTime = DateTime.Now;
         Debug.Log("Counter: " + counter.ToString());
         motionDB.Build();
+        DateTime buildTime = DateTime.Now;
+        Debug.Log("Time to ingest: " + (ingestTime - startTime).Milliseconds);
+        Debug.Log("Time to Build: " + (buildTime - ingestTime).Milliseconds);
 
+    }
+
+    private void normalizeVector(double[] vec)
+    {
+        for(int i = 0; i < 30; i ++)
+        {
+            vec[i] = (vec[i] - means[i]) / std_devs[i];
+        }
     }
     void Start()
     {
